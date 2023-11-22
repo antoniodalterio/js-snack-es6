0513250fc8e9efbb72c90d15bcb9ad73d069aa1f
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-22 15:00:29 +0100

    feat: array object bici

--- Snack 1/main.js
+++ Snack 1/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+/* 
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal 
+*/
+
+function numeroRandom() {
+  const num = Math.floor(Math.random() * 30 + 10);
+
+  return num;
+}
+
+console.log(numeroRandom());
+
+const bici = [
+  {
+    nome: 'Bici da città',
+    peso: numeroRandom(),
+  },
+  {
+    nome: 'Mountain bike',
+    peso: numeroRandom(),
+  },
+  {
+    nome: 'Bmx',
+    peso: numeroRandom(),
+  },
+  {
+    nome: 'Bici da cicloturismo',
+    peso: numeroRandom(),
+  },
+  {
+    nome: 'Downhill',
+    peso: numeroRandom(),
+  },
+];
+
+console.log(bici);
